baa10ef14065bd131226d6f7b696bdc19549e4e1
k-kahora <mango22mjk@gmail.com>
2025-04-23 10:01:19 -0400

    VPC setupgit status

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
 terraform {
+  # cloud {
+  #   organization = "PKI-Industries"
+  #   workspaces {
+  #     name = "learn-terraform-aws"
+  #   }
+  # }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -10,15 +16,33 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-west-2"
-  profile = "admin-profile"
+  region  = "us-east-1"
+  profile = "admin"
 }
-
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
-
+resource "aws_vpc" "main" {
+  cidr_block                       = "10.16.0.0/16"
+  assign_generated_ipv6_cidr_block = true
+  enable_dns_hostnames             = true
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = "Main VPC"
   }
 }
+data "aws_vpc" "main" {
+  id = aws_vpc.main.id
+
+}
+resource "aws_subnet" "private_subnets" {
+  for_each = var.subnets
+
+  vpc_id                          = aws_vpc.main.id
+  cidr_block                      = each.value.cidr_block
+  availability_zone               = each.value.availability_zone
+  assign_ipv6_address_on_creation = true
+  ipv6_cidr_block                 = cidrsubnet(aws_vpc.main.ipv6_cidr_block, 8, each.value.custom_ipv6)
+  tags = {
+    Name         = each.key
+    CustomIPV6ID = format("%02X", each.value.custom_ipv6)
+  }
+
+}
+
---
+++ variables.tf
@@ -0,0 +1,41 @@
+variable "instance_name" {
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string
+  default     = "ExampleAppServerInstance"
+}
+variable "private_subnet_cidrs" {
+  type        = list(string)
+  description = "Private Subnet CIDR values"
+  default     = ["10.16.0.0/20", "10.16.16.0/20", "10.16.32.0/20", "10.16.48.0/20"]
+}
+variable "azs" {
+  type        = list(string)
+  description = "availability zones"
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+
+}
+
+
+variable "subnets" {
+  type = map(object({
+    cidr_block        = string
+    availability_zone = string
+    custom_ipv6       = number # use 0 to 11
+  }))
+  default = {
+    sn-reserved-A = { cidr_block = "10.16.0.0/20", availability_zone = "us-east-1a", custom_ipv6 = 0 }
+    sn-db-A       = { cidr_block = "10.16.16.0/20", availability_zone = "us-east-1a", custom_ipv6 = 1 }
+    sn-app-A      = { cidr_block = "10.16.32.0/20", availability_zone = "us-east-1a", custom_ipv6 = 2 }
+    sn-web-A      = { cidr_block = "10.16.48.0/20", availability_zone = "us-east-1a", custom_ipv6 = 3 }
+
+    sn-reserved-B = { cidr_block = "10.16.64.0/20", availability_zone = "us-east-1b", custom_ipv6 = 4 }
+    sn-db-B       = { cidr_block = "10.16.80.0/20", availability_zone = "us-east-1b", custom_ipv6 = 5 }
+    sn-app-B      = { cidr_block = "10.16.96.0/20", availability_zone = "us-east-1b", custom_ipv6 = 6 }
+    sn-web-B      = { cidr_block = "10.16.112.0/20", availability_zone = "us-east-1b", custom_ipv6 = 7 }
+
+    sn-reserved-C = { cidr_block = "10.16.128.0/20", availability_zone = "us-east-1c", custom_ipv6 = 8 }
+    sn-db-C       = { cidr_block = "10.16.144.0/20", availability_zone = "us-east-1c", custom_ipv6 = 9 }
+    sn-app-C      = { cidr_block = "10.16.160.0/20", availability_zone = "us-east-1c", custom_ipv6 = 10 }
+    sn-web-C      = { cidr_block = "10.16.176.0/20", availability_zone = "us-east-1c", custom_ipv6 = 11 }
+  }
+}
